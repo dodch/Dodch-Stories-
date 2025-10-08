55500cb703e49c66bc20bd4ee2bbef5fdf262c84
dodch <dotchadam@gmail.com>
2025-10-08 22:54:30 +0100

    5

--- index.html/script.js
+++ index.html/script.js
@@ -194,14 +194,16 @@ async function fetchAndBuildGrid() {
         <span class="story-length">${story.length}</span>
         <span class="creation-date">${story.date}</span>
         <p class="story-creator">made by ${story.creator}</p>
-        <div class="favorite-btn glass-button-base">
+        <div class="favorite-container">
+          <span class="favorite-count">0</span>
+          <div class="favorite-btn glass-button-base">
           <svg viewBox="0 0 24 24">
             <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 
             4.42 3 7.5 3c1.74 0 3.41 0.81 4.5 2.09C13.09 3.81 14.76 3 16.5 
             3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
           </svg>
           <!-- NEW: Span to display the local favorite count -->
-          <span class="favorite-count">0</span>
+          </div>
         </div>
       `;
       const status = story.status ? story.status : '';
